50faa5e83bbe77c4f5c80273042402089259390a
supabase2026 <supabase2026@ytonihon.com.br>
2026-04-25 15:17:50 -0300

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://brlrczrhcgfrpzgjxetg.supabase.co';
+const supabaseKey = 'sb_publishable_1fWbAhxS4wp4Qn1K78HnRA_CB_PjAni';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
