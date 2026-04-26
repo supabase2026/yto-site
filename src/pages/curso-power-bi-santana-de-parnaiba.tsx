f0fc6ba54cd68faa49d7216bc20081620a578992
supabase2026 <supabase2026@ytonihon.com.br>
2026-04-26 19:41:59 -0300

    Create curso-power-bi-santana-de-parnaiba.tsx

---
+++ src/pages/curso-power-bi-santana-de-parnaiba.tsx
@@ -0,0 +1,29 @@
+// src/pages/curso-power-bi-santana-de-parnaiba.tsx
+
+import SeoLocalPage from "./SeoLocalPage";
+
+const CursoPowerBISantanaDeParnaiba = () => (
+  <SeoLocalPage
+    titulo="Curso de Power BI em Santana de Parnaíba"
+    subtitulo="Aprenda Power BI com o Grupo Yto Nihon e desenvolva dashboards profissionais para análise de dados, gestão e tomada de decisão."
+    cidade="Santana de Parnaíba"
+    curso="curso de Power BI"
+    publico={[
+      "Profissionais que querem atuar com dados",
+      "Analistas que desejam criar painéis gerenciais",
+      "Gestores que acompanham resultados",
+      "Empresas de Santana de Parnaíba e região",
+    ]}
+    conteudos={[
+      "Power Query para tratamento de dados",
+      "Modelagem no Power BI",
+      "Criação de indicadores",
+      "Dashboards profissionais",
+      "Publicação de relatórios",
+    ]}
+    empresas
+    cta="Quero aprender Power BI em Santana de Parnaíba"
+  />
+);
+
+export default CursoPowerBISantanaDeParnaiba;
