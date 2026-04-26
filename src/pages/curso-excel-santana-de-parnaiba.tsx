b211f3198bdccbd4a4401e9f34a1ca1e72dc7fea
supabase2026 <supabase2026@ytonihon.com.br>
2026-04-26 19:41:39 -0300

    Create curso-excel-santana-de-parnaiba.tsx

---
+++ src/pages/curso-excel-santana-de-parnaiba.tsx
@@ -0,0 +1,29 @@
+// src/pages/curso-excel-santana-de-parnaiba.tsx
+
+import SeoLocalPage from "./SeoLocalPage";
+
+const CursoExcelSantanaDeParnaiba = () => (
+  <SeoLocalPage
+    titulo="Curso de Excel em Santana de Parnaíba"
+    subtitulo="O Grupo Yto Nihon oferece curso de Excel para alunos e empresas de Santana de Parnaíba, com foco prático em produtividade, relatórios e análise de dados."
+    cidade="Santana de Parnaíba"
+    curso="curso de Excel"
+    publico={[
+      "Profissionais que querem dominar o Excel",
+      "Estudantes em busca de qualificação",
+      "Empresas que precisam melhorar processos internos",
+      "Pessoas que trabalham com controles e relatórios",
+    ]}
+    conteudos={[
+      "Planilhas profissionais",
+      "Fórmulas e funções do Excel",
+      "Validação e organização de dados",
+      "Tabelas dinâmicas",
+      "Relatórios para tomada de decisão",
+    ]}
+    empresas
+    cta="Quero aprender Excel em Santana de Parnaíba"
+  />
+);
+
+export default CursoExcelSantanaDeParnaiba;
