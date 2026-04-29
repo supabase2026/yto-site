c1b15d233ee5c7964b99d66d02e23e1d43d836d9
supabase2026 <supabase2026@ytonihon.com.br>
2026-04-29 09:21:48 -0300

    Create treinamentos-barueri-alphaville-regiao.tsx

---
+++ src/pages/treinamentos-barueri-alphaville-regiao.tsx
@@ -0,0 +1,42 @@
+import SeoLocalPage from "./SeoLocalPage";
+
+const TreinamentosBarueriAlphavilleRegiao = () => (
+  <SeoLocalPage
+    titulo="Treinamentos em Barueri, Alphaville e Região"
+    subtitulo="Cursos e treinamentos profissionais de Excel, Power BI e tecnologia para alunos, profissionais e empresas em Barueri, Alphaville, Osasco e Santana de Parnaíba."
+    cidade="Barueri e Alphaville"
+    curso="Treinamentos Profissionais"
+    descricao="O Grupo Yto Nihon oferece treinamentos práticos e objetivos para quem deseja evoluir profissionalmente com Excel, Power BI, análise de dados e ferramentas corporativas. Atendemos alunos individuais e empresas de Barueri, Alphaville, Osasco, Santana de Parnaíba e região."
+    beneficios={[
+      "Treinamentos presenciais e online ao vivo",
+      "Conteúdo prático voltado ao mercado de trabalho",
+      "Cursos para alunos, profissionais e empresas",
+      "Atendimento para Barueri, Alphaville, Osasco e Santana de Parnaíba",
+      "Instrutores experientes e metodologia aplicada",
+      "Foco em produtividade, dados e desenvolvimento profissional",
+    ]}
+    publico={[
+      "Profissionais que desejam melhorar no Excel",
+      "Analistas que querem aprender Power BI",
+      "Empresas que precisam treinar suas equipes",
+      "Pessoas em transição para a área de dados",
+      "Alunos iniciantes em tecnologia e análise de dados",
+    ]}
+    modulos={[
+      "Curso de Excel em Barueri",
+      "Curso de Excel em Alphaville",
+      "Curso de Excel em Osasco",
+      "Curso de Excel em Santana de Parnaíba",
+      "Curso de Power BI em Barueri",
+      "Curso de Power BI em Alphaville",
+      "Curso de Power BI em Osasco",
+      "Curso de Power BI em Santana de Parnaíba",
+      "Treinamentos corporativos para empresas",
+    ]}
+    chamadaFinal="Fale com o Grupo Yto Nihon e descubra qual treinamento é mais indicado para você ou para sua empresa."
+    botaoTexto="Falar com a Yto pelo WhatsApp"
+    botaoLink="https://wa.me/5511910704164"
+  />
+);
+
+export default TreinamentosBarueriAlphavilleRegiao;
