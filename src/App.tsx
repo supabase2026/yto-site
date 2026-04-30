6574f6a45631a3aac434a672c4c746d9bb6930bf
supabase2026 <supabase2026@ytonihon.com.br>
2026-04-30 16:53:06 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Diagnostico from "./pages/Diagnostico";
 import AreaDoAluno from "./pages/AreaDoAluno";
 import CursoMSProject from "./pages/curso-ms-project";
 import CursoMSProjectAlphaville from "./pages/curso-ms-project-alphaville";
@@ -114,6 +115,7 @@ const App = () => (
           <Route path="/curso-ms-project-alphaville" element={<CursoMSProjectAlphaville />} />
           <Route path="/curso-ms-project" element={<CursoMSProject />} />
           <Route path="/area-do-aluno" element={<AreaDoAluno />} />
+          <Route path="/diagnostico" element={<Diagnostico />} />
         </Routes>
   
       </BrowserRouter>
