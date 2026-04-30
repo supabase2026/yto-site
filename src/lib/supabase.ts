0e6b710e08993765b3d82de0250a5a66138cec96
supabase2026 <supabase2026@ytonihon.com.br>
2026-04-30 18:09:22 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
+
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://brlrczzhcgfrpzgxjetg.supabase.co";
+
 const supabaseKey = "sb_publishable_1fWbAhxS4wp4Qn1K78HnRA_CB_PjAni";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
